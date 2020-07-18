c9f009ba800880d6eeaefdf0bc38fde46867c16d
marlboroKay <18926560772@163.com>
2020-07-18 23:55:04 +0800

    '项目结构划分'

---
+++ src/components/content/mainTabbar/mainTabbar.vue
@@ -0,0 +1,41 @@
+<template>
+  <tab-bar>
+    <tab-bar-item path="/home">
+      <img slot="item-icon" src="~assets/img/home/home.svg" alt="">
+      <img slot="item-active-icon" src="~assets/img/home/home_active.svg" alt="">
+      <span slot="item-text">首页</span>
+    </tab-bar-item>
+    <tab-bar-item path="/category">
+      <img slot="item-icon" src="~assets/img/category/category.svg" alt="">1
+      <img slot="item-active-icon" src="~assets/img/category/category_active.svg" alt="">
+      <span slot="item-text">分类</span>
+    </tab-bar-item>
+    <tab-bar-item path="/cart">
+      <img slot="item-icon" src="~assets/img/cart/shopcart.svg" alt="">
+      <img slot="item-active-icon" src="~assets/img/cart/shopcart_active.svg" alt="">
+      <span slot="item-text">购物车</span>
+    </tab-bar-item>
+    <tab-bar-item path="/profile">
+      <img slot="item-icon" src="~assets/img/profile/profile.svg" alt="">
+      <img slot="item-active-icon" src="~assets/img/profile/profile_active.svg" alt="">
+      <span slot="item-text">我的</span>
+    </tab-bar-item>
+  </tab-bar>
+</template>
+
+<script>
+  import TabBar from "components/common/Tabbar/TabBar";
+  import TabBarItem from "components/common/Tabbar/TabBarItem";
+
+  export default {
+    name: "MainTabBar",
+    components: {
+      TabBar,
+      TabBarItem
+    }
+  }
+</script>
+
+<style scoped>
+
+</style>
