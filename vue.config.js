a767c2da37bc7b38855d3fc2f85712be46e27d4b
marlboroKay <18926560772@163.com>
2020-07-18 19:00:14 +0800

    '增加配置文件'

---
+++ vue.config.js
@@ -0,0 +1,15 @@
+module.exports = {
+  configureWebpack:{
+    resolve:{
+      alias:{
+        'assets':'@/assets',
+        'common':'@/common',
+        'const':'@/const',
+        'views':'@/views',
+        'components':'@/components',
+        'network':'@/network'
+      }
+    }
+  }
+
+}
